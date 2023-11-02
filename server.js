66b05e7cd59c34e79c87bbade774c7cd5695a785
Hérick Bittencourt <professional.herickvvb@gmail.com>
2023-11-02 12:11:05 -0300

    PORT agora é uma variavel

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const website = require('./src/routes/website');
 const dbAPI = require('./src/routes/database');
 
-app.listen(3000, () => {
+const PORT = 3000
+
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
 
