044b4a9dc2bda58cd1cb0f5ea76eee1cc16200bc
Hérick Bittencourt <professional.herickvvb@gmail.com>
2023-11-20 20:56:26 -0300

    Server can now use public folder

--- server.js
+++ server.js
@@ -10,10 +10,21 @@ app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 });
 
+// Define 'views' para começar pela pasta src
 app.set('views', 'src');
+
+// Troca o caminho da pasta public (contém imagens, css, etc)
+// para src/public
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('src/public'));
+
+// Define a engine de renderização para .ejs
 app.set('view engine', 'ejs');
 
+// Permite o uso de .json e a pasta public
 app.use(express.json());
+app.use(express.static('public'));
+
 // Conexão das rotas ao servidor aplicativo express
 // OBS: website deve ser o ultimo router posicionado
 //      pois nele contém os erros 404 e 405 no fim,
